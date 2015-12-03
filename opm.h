b4a62644a812bd4bc40f4930f3fdb56d6f73cc4d
Hirofumi Katayama <katayama.hirofumi.mz@gmail.com>
2015-12-03 19:55:28 +0900

    update opm.h

--- opm.h
+++ opm.h
@@ -71,96 +71,96 @@
 //      irq = true:  IRQ óvãÅÇ™î≠ê∂
 //      irq = false: IRQ óvãÅÇ™è¡Ç¶ÇÈ
 //
-namespace FM
-{
-    //  YM2151(OPM) ----------------------------------------------------
-    class OPM : public Timer
-    {
-    public:
-        OPM();
-        ~OPM() {}
+namespace FM {
 
-        bool        Init(uint32_t c, uint32_t r, bool=false);
-        bool        SetRate(uint32_t c, uint32_t r, bool);
-        void        SetLPFCutoff(uint32_t freq);
-        void        Reset();
-        
-        void        SetReg(uint32_t addr, uint32_t data);
-        uint32_t    GetReg(uint32_t addr);
-        uint32_t    ReadStatus() {
-            return (status & 0x03);
-        }
-        
-        void        Mix(Sample* buffer, int nsamples);
-        
-        void        SetVolume(int db);
-        void        SetChannelMask(uint32_t mask);
-        
-    private:
-        virtual void Intr(bool) {}
+//  YM2151(OPM) ----------------------------------------------------
+class OPM : public Timer {
+public:
+    OPM();
+    ~OPM() { }
+
+    bool        Init(uint32_t c, uint32_t r, bool=false);
+    bool        SetRate(uint32_t c, uint32_t r, bool);
+    void        SetLPFCutoff(uint32_t freq);
+    void        Reset();
     
-    private:
-        enum
-        {
-            OPM_LFOENTS = 512,
-        };
-        
-        void        SetStatus(uint32_t bit);
-        void        ResetStatus(uint32_t bit);
-        void        SetParameter(uint32_t addr, uint32_t data);
-        void        TimerA();
-        void        RebuildTimeTable();
-        void        MixSub(int activech, ISample**);
-        void        MixSubL(int activech, ISample**);
-        void        LFO();
-        uint32_t    Noise();
-        
-        int         fmvolume;
+    void        SetReg(uint32_t addr, uint32_t data);
+    uint32_t    GetReg(uint32_t addr);
+    uint32_t    ReadStatus() {
+        return (status & 0x03);
+    }
+    
+    void        Mix(Sample* buffer, int nsamples);
+    
+    void        SetVolume(int db);
+    void        SetChannelMask(uint32_t mask);
+    
+private:
+    virtual void Intr(bool) {}
 
-        uint32_t    clock;
-        uint32_t    rate;
-        uint32_t    pcmrate;
-
-        uint32_t    pmd;
-        uint32_t    amd;
-        uint32_t    lfocount;
-        uint32_t    lfodcount;
-
-        uint32_t    lfo_count_;
-        uint32_t    lfo_count_diff_;
-        uint32_t    lfo_step_;
-        uint32_t    lfo_count_prev_;
-
-        uint32_t    lfowaveform;
-        uint32_t    rateratio;
-        uint32_t    noise;
-        int32_t     noisecount;
-        uint32_t    noisedelta;
-        
-        bool        interpolation;
-        uint8_t     lfofreq;
-        uint8_t     status;
-        uint8_t     reg01;
-
-        uint8_t     kc[8];
-        uint8_t     kf[8];
-        uint8_t     pan[8];
-
-        Channel4    ch[8];
-        Chip        chip;
-
-        static void BuildLFOTable();
-        static int amtable[4][OPM_LFOENTS];
-        static int pmtable[4][OPM_LFOENTS];
-
-    public:
-        int     dbgGetOpOut(int c, int s) {
-            return ch[c].op[s].dbgopout_;
-        }
-        Channel4* dbgGetCh(int c) {
-            return &ch[c];
-        }
+private:
+    enum
+    {
+        OPM_LFOENTS = 512,
     };
+    
+    void        SetStatus(uint32_t bit);
+    void        ResetStatus(uint32_t bit);
+    void        SetParameter(uint32_t addr, uint32_t data);
+    void        TimerA();
+    void        RebuildTimeTable();
+    void        MixSub(int activech, ISample**);
+    void        MixSubL(int activech, ISample**);
+    void        LFO();
+    uint32_t    Noise();
+    
+    int         fmvolume;
+
+    uint32_t    clock;
+    uint32_t    rate;
+    uint32_t    pcmrate;
+
+    uint32_t    pmd;
+    uint32_t    amd;
+    uint32_t    lfocount;
+    uint32_t    lfodcount;
+
+    uint32_t    lfo_count_;
+    uint32_t    lfo_count_diff_;
+    uint32_t    lfo_step_;
+    uint32_t    lfo_count_prev_;
+
+    uint32_t    lfowaveform;
+    uint32_t    rateratio;
+    uint32_t    noise;
+    int32_t     noisecount;
+    uint32_t    noisedelta;
+    
+    bool        interpolation;
+    uint8_t     lfofreq;
+    uint8_t     status;
+    uint8_t     reg01;
+
+    uint8_t     kc[8];
+    uint8_t     kf[8];
+    uint8_t     pan[8];
+
+    Channel4    ch[8];
+    Chip        chip;
+
+    static void BuildLFOTable();
+    static int amtable[4][OPM_LFOENTS];
+    static int pmtable[4][OPM_LFOENTS];
+
+public:
+    int     dbgGetOpOut(int c, int s) {
+        return ch[c].op[s].dbgopout_;
+    }
+    Channel4* dbgGetCh(int c) {
+        return &ch[c];
+    }
+};
+
 } // namespace FM
 
 #endif  // ndef FMGON_OPM_H
