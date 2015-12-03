8773f66e5ad21288ca4dc2da01bcc23612de738e
Hirofumi Katayama <katayama.hirofumi.mz@gmail.com>
2015-12-03 19:54:18 +0900

    update fmtimer.h

--- fmtimer.h
+++ fmtimer.h
@@ -4,50 +4,48 @@
 // ---------------------------------------------------------------------------
 //  $Id: fmtimer.h,v 1.2 2003/04/22 13:12:53 cisc Exp $
 
-#ifndef FMGON_TIMER_H
-#define FMGON_TIMER_H
+#ifndef FMGON_FMTIMER_H
+#define FMGON_FMTIMER_H
 
 // ---------------------------------------------------------------------------
 
-namespace FM
-{
-    class Timer
-    {
-    public:
-        void    Reset();
-        bool    Count(int32_t us);
-        int32_t GetNextEvent();
+namespace FM {
 
-    protected:
-        virtual void SetStatus(uint32_t bit) = 0;
-        virtual void ResetStatus(uint32_t bit) = 0;
+class Timer {
+public:
+    void    Reset();
+    bool    Count(int32_t us);
+    int32_t GetNextEvent();
 
-        void    SetTimerBase(uint32_t clock);
-        void    SetTimerA(uint32_t addr, uint32_t data);
-        void    SetTimerB(uint32_t data);
-        void    SetTimerControl(uint32_t data);
+protected:
+    virtual void SetStatus(uint32_t bit) = 0;
+    virtual void ResetStatus(uint32_t bit) = 0;
 
-        uint8_t status;
-        uint8_t regtc;
+    void    SetTimerBase(uint32_t clock);
+    void    SetTimerA(uint32_t addr, uint32_t data);
+    void    SetTimerB(uint32_t data);
+    void    SetTimerControl(uint32_t data);
 
-    private:
-        virtual void TimerA() {}
-        uint8_t regta[2];
+    uint8_t status;
+    uint8_t regtc;
 
-        int32_t timera, timera_count;
-        int32_t timerb, timerb_count;
-        int32_t timer_step;
-    };
+private:
+    virtual void TimerA() {}
+    uint8_t regta[2];
+
+    int32_t timera, timera_count;
+    int32_t timerb, timerb_count;
+    int32_t timer_step;
+};
 
 // ---------------------------------------------------------------------------
 //  èâä˙âª
 //
-inline void Timer::Reset()
-{
+inline void Timer::Reset() {
     timera_count = 0;
     timerb_count = 0;
 }
 
 } // namespace FM
 
-#endif  // ndef FMGON_TIMER_H
+#endif  // ndef FMGON_FMTIMER_H
