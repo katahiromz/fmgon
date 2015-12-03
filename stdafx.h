dc1dc9ebf0eee5e694bc769d69d315dd5b1f89f8
Hirofumi Katayama <katayama.hirofumi.mz@gmail.com>
2015-12-03 16:46:59 +0900

    update fmgeninl.h and stdafx.h

--- stdafx.h
+++ stdafx.h
@@ -1,3 +1,14 @@
+// C++ headers
+#include <vector>   // for std::vector
+#include <deque>    // for std::deque
+#include <thread>   // for std::thread
+#include <mutex>    // for std::mutex
+#include <memory>   // for smart pointers
+using std::shared_ptr;
+using std::make_shared;
+using std::unique_ptr;
+
+// C headers
 #include <cstdlib>
 #include <cstdio>
 #include <cmath>
